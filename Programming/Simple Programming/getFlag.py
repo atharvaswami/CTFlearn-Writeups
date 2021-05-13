2ea34808517078d098dea7761700e64c4a7d43e9
Atharva Swami <62474334+atharvaswami@users.noreply.github.com>
2021-05-14 00:35:59 +0530

    Update getFlag.py

--- Programming/Simple Programming/getFlag.py
+++ Programming/Simple Programming/getFlag.py
@@ -1,11 +1,11 @@
-ans=0
+flag=0
 with open('data.dat') as F:
     L = F.readlines()  
     for S in L:
         try:
             if(S.count('0')%3==0 or S.count('1')%2==0):
-                ans+=1
+                flag+=1
         except EOFError:
             break
-print("CTFlearn{" + str(ans) + "}")
+print("CTFlearn{" + str(flag) + "}")
 F.close()
